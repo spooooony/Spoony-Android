8fe6171c7c8c99ca171510a495630c9dc2d44913
roel <ash49901@gmail.com>
2025-01-04 14:48:14 +0900

    [FEAT/#5] Pretendard Font 추가

---
+++ app/src/main/java/com/spoony/spoony/core/designsystem/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.spoony.spoony.core.designsystem.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.spoony.spoony.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+)
