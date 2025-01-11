bb3aca6f84f009ef92cd47a30c00d8384ebb3096
roel <ash49901@gmail.com>
2025-01-11 21:19:29 +0900

    [FEAT/#11] BaseDialog UI 구현

---
+++ app/src/main/java/com/spoony/spoony/core/components/dialog/BaseDialog.kt
@@ -0,0 +1,58 @@
+package com.spoony.spoony.core.components.dialog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun BaseDialog(
+    message: String,
+    onDismiss: () -> Unit = {},
+    buttons: @Composable () -> Unit,
+    content: @Composable () -> Unit = {}
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(horizontal = 20.dp)
+                .background(
+                    color = SpoonyAndroidTheme.colors.white,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(
+                    top = 40.dp,
+                    bottom = 20.dp,
+                    start = 20.dp,
+                    end = 20.dp
+                )
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                content()
+                Text(
+                    text = message,
+                    style = SpoonyAndroidTheme.typography.body1b,
+                    textAlign = TextAlign.Center
+                )
+                buttons()
+            }
+        }
+    }
+}
