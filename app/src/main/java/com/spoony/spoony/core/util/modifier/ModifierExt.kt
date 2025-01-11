1b586e0bce302e764ac0a116a437e0ed812adafd
roel <ash49901@gmail.com>
2025-01-11 23:45:18 +0900

    [FEAT/#12] Modifier.noRippleClickable 구현

---
+++ app/src/main/java/com/spoony/spoony/core/util/modifier/ModifierExt.kt
@@ -0,0 +1,19 @@
+package com.spoony.spoony.core.util.modifier
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+@Composable
+fun Modifier.noRippleClickable(
+    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+): Modifier = composed {
+    this.clickable(
+        indication = null,
+        interactionSource = interactionSource
+    ) { onClick() }
+}
