41a2c85ef870b003d01533d7cfd4e814a0c071bc
roel <ash49901@gmail.com>
2025-01-15 04:25:50 +0900

    [MOD/#27] import 수정

--- app/src/main/java/com/spoony/spoony/core/util/extension/ColorExt.kt
+++ app/src/main/java/com/spoony/spoony/core/util/extension/ColorExt.kt
@@ -1,8 +1,9 @@
 package com.spoony.spoony.core.util.extension
 
+import android.graphics.Color.parseColor
 import androidx.compose.ui.graphics.Color
 
 fun Color.Companion.hexToColor(hex: String): Color {
-    val colorInt = android.graphics.Color.parseColor("#$hex")
+    val colorInt = parseColor("#$hex")
     return Color(colorInt)
 }
