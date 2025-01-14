fe4907563061c9f527b36af46b2ee8e011a18f9f
roel <ash49901@gmail.com>
2025-01-15 05:22:50 +0900

    [FEAT/#29] 장소 상세 Profile 구현

---
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/component/Profile.kt
@@ -0,0 +1,98 @@
+package com.spoony.spoony.presentation.placeDetail.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.spoony.spoony.R
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+import com.spoony.spoony.core.util.extension.noRippleClickable
+
+@Composable
+fun Profile(
+    imageUrl: String,
+    name: String,
+    location: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(14.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(
+                    color = SpoonyAndroidTheme.colors.gray500,
+                    shape = CircleShape
+                ),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = name,
+                style = SpoonyAndroidTheme.typography.body2b,
+                color = SpoonyAndroidTheme.colors.black
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = location,
+                style = SpoonyAndroidTheme.typography.caption1m,
+                color = SpoonyAndroidTheme.colors.gray400
+            )
+        }
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_menu_24),
+            contentDescription = null,
+            tint = Color.Unspecified,
+            modifier = Modifier
+                .noRippleClickable(onClick)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ProfilePreview() {
+    SpoonyAndroidTheme {
+        Profile(
+            imageUrl = "https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg",
+            name = "클레오가트라",
+            location = "마포구 수저",
+            onClick = {}
+        )
+    }
+}
