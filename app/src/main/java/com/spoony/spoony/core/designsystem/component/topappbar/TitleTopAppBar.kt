9a7f16e86acc7aaf2a8afce803891df3487ae6c4
Hyobeen-Park <amanda0109@naver.com>
2025-01-15 07:31:49 +0900

    [FEAT/#23] 제목 탑앱바 구현

---
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/topappbar/TitleTopAppBar.kt
@@ -0,0 +1,38 @@
+package com.spoony.spoony.core.designsystem.component.topappbar
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun TitleTopAppBar(
+    title: String,
+    onBackButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    SpoonyBasicTopAppBar(
+        showBackButton = true,
+        onBackButtonClick = onBackButtonClick,
+        content = {
+            Text(
+                text = title,
+                style = SpoonyAndroidTheme.typography.title2b,
+                color = SpoonyAndroidTheme.colors.black
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun TitleTopAppBarPreview() {
+    SpoonyAndroidTheme {
+        TitleTopAppBar(
+            title = "신고하기",
+            onBackButtonClick = {}
+        )
+    }
+}
