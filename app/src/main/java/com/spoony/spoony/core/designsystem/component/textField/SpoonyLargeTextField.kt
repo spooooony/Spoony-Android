33605bf4347c787e3ce3e849faf28edca658fff8
MinJae Han <hanminjae131@gmail.com>
2025-01-14 19:00:44 +0900

    [FEAT/#15] SpoonyLargeTextField 구현

---
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/textField/SpoonyLargeTextField.kt
@@ -0,0 +1,218 @@
+package com.spoony.spoony.core.designsystem.component.textField
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spoony.spoony.R
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun SpoonyLargeTextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    maxLength: Int = Int.MAX_VALUE,
+    minLength: Int = 0,
+    minErrorText: String? = null,
+    maxErrorText: String? = null
+) {
+    var isFocused by remember { mutableStateOf(false) }
+    var isError: Boolean by remember { mutableStateOf(false) }
+    var selectText: Boolean by remember { mutableStateOf(false) }
+    val spoonyColors = SpoonyAndroidTheme.colors
+
+    val borderColor = remember(isError, isFocused) {
+        when {
+            isError -> spoonyColors.error400
+            isFocused -> spoonyColors.main400
+            else -> spoonyColors.gray100
+        }
+    }
+
+    val subContentColor = remember(isError) {
+        when {
+            isError -> spoonyColors.error400
+            else -> spoonyColors.gray500
+        }
+    }
+
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        CustomBasicTextField(
+            value = value,
+            placeholder = placeholder,
+            onValueChanged = { newText ->
+                isError = (newText.length > maxLength || newText.trim().length < minLength)
+                selectText = newText.length > maxLength
+                if (newText.length <= maxLength) {
+                    onValueChanged(newText)
+                }
+            },
+            maxLength = maxLength,
+            borderColor = borderColor,
+            subContentColor = subContentColor,
+            modifier = modifier,
+            onFocusChanged = {
+                isFocused = it
+                if (value.length >= minLength) {
+                    isError = false
+                }
+            }
+        )
+
+        if (((minErrorText != null || maxErrorText != null) && isError)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_error_24),
+                    contentDescription = null,
+                    tint = subContentColor,
+                    modifier = Modifier.size(16.dp)
+                )
+                (if(selectText) maxErrorText else minErrorText)?.let {
+                    Text(
+                        text = it,
+                        style = SpoonyAndroidTheme.typography.caption1m,
+                        color = subContentColor
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CustomBasicTextField(
+    value: String,
+    placeholder: String,
+    onValueChanged: (String) -> Unit,
+    borderColor: Color,
+    onFocusChanged: (Boolean) -> Unit,
+    subContentColor: Color,
+    modifier: Modifier = Modifier,
+    maxLength: Int = 0,
+    backgroundColor: Color = SpoonyAndroidTheme.colors.white,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false
+) {
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChanged,
+            modifier = modifier
+                .clip(RoundedCornerShape(8.dp))
+                .border(
+                    1.dp,
+                    borderColor,
+                    RoundedCornerShape(8.dp)
+                )
+                .background(backgroundColor)
+                .padding(12.dp)
+                .focusRequester(focusRequester)
+                .onFocusChanged { focusState ->
+                    onFocusChanged(focusState.isFocused)
+                },
+            singleLine = singleLine,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                },
+                onSearch = {
+                    focusManager.clearFocus()
+                }
+            ),
+            textStyle = SpoonyAndroidTheme.typography.body2m.copy(
+                color = SpoonyAndroidTheme.colors.gray900
+            ),
+            decorationBox = { innerTextField ->
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(80.dp)
+                    ) {
+                        innerTextField()
+                        if (value.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                color = SpoonyAndroidTheme.colors.gray500,
+                                style = SpoonyAndroidTheme.typography.body2m
+                            )
+                        }
+                    }
+                    Text(
+                        text = "${value.length} / $maxLength",
+                        style = SpoonyAndroidTheme.typography.caption1m,
+                        color = subContentColor,
+                        modifier = Modifier.align(Alignment.End)
+                    )
+                }
+            }
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SpoonyLargeTextFieldPreview() {
+    var text by remember { mutableStateOf("") }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        SpoonyAndroidTheme {
+            SpoonyLargeTextField(
+                value = text,
+                placeholder = "장소명 언급은 피해주세요. 우리만의 비밀!",
+                onValueChanged = { newText ->
+                    text = newText
+                },
+                maxLength = 500,
+                minLength = 50,
+                minErrorText = "자세한 후기는 필수예요",
+                maxErrorText = "글자 수 500자 이하로 입력해 주세요"
+            )
+        }
+    }
+}
