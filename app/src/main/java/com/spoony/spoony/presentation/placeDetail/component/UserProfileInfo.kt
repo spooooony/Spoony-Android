71d0ca33bb1b55366d4eeb559c410f844857f957
roel <ash49901@gmail.com>
2025-01-15 14:47:12 +0900

    [FEAT/#29] UserProfileInfo maxLines 및 overflow 설정

--- app/src/main/java/com/spoony/spoony/presentation/placeDetail/component/UserProfileInfo.kt
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/component/UserProfileInfo.kt
@@ -5,20 +5,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.spoony.spoony.R
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
 
 @Composable
@@ -54,13 +61,17 @@ fun UserProfileInfo(
             Text(
                 text = name,
                 style = SpoonyAndroidTheme.typography.body2b,
-                color = SpoonyAndroidTheme.colors.black
+                color = SpoonyAndroidTheme.colors.black,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
             Spacer(modifier = Modifier.height(4.dp))
             Text(
                 text = location,
                 style = SpoonyAndroidTheme.typography.caption1m,
-                color = SpoonyAndroidTheme.colors.gray400
+                color = SpoonyAndroidTheme.colors.gray400,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
         }
     }
@@ -75,5 +86,29 @@ private fun UserProfileInfoPreview() {
             name = "클레오가트라",
             location = "마포구 수저"
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun UserProfileInfoMenuPreview() {
+    SpoonyAndroidTheme {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(14.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            UserProfileInfo(
+                imageUrl = "https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg",
+                name = "클레오가트라클레오가트라클레오가트라클레오가트라클레오가트라클레오가트라클레오가트라클레오가트라클레오가트라클레오가트라",
+                location = "마포구 수저마포구 수저마포구 수저마포구 수저마포구 수저마포구 수저마포구 수저마포구 수저마포구 수저",
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_menu_24),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
     }
 }
