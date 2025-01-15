4e849e147b496e9cf3095e9fd5f3ffb090457c2d
roel <ash49901@gmail.com>
2025-01-16 04:32:55 +0900

    [Feat/#39] 신고하기 페이지 ReportRadioButton 컴포넌트 구현

---
+++ app/src/main/java/com/spoony/spoony/presentation/report/component/ReportRadioButton.kt
@@ -0,0 +1,72 @@
+package com.spoony.spoony.presentation.report.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+import com.spoony.spoony.core.util.extension.noRippleClickable
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.immutableListOf
+
+@Composable
+fun ReportRadioButton (
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit,
+    options: ImmutableList<String>
+) {
+    Column {
+        options.forEach { option ->
+            Row(
+                modifier = Modifier.padding(vertical = 9.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    modifier = Modifier.size(24.dp),
+                    selected = selectedOption == option,
+                    onClick = { onOptionSelected(option) },
+                    colors = RadioButtonDefaults.colors(
+                        selectedColor = SpoonyAndroidTheme.colors.black,
+                        unselectedColor = SpoonyAndroidTheme.colors.gray400,
+                    )
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Text(
+                    text = option,
+                    modifier = Modifier.noRippleClickable { onOptionSelected(option) },
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ReportRadioButtonPreview() {
+    var selectedOption by remember { mutableStateOf("영리 목적/ 홍보성 후기") }
+    val options = immutableListOf("영리 목적/ 홍보성 후기", "욕설/ 인신 공격", "불법정보", "개인 정보 노출", "도배", "기타")
+    SpoonyAndroidTheme {
+        ReportRadioButton(
+            selectedOption = selectedOption,
+            onOptionSelected = { selectedOption = it },
+            options = options
+        )
+    }
+}
