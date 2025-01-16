c847d26441ec072247913696e64c0b46ec763e0f
Hyobeen-Park <amanda0109@naver.com>
2025-01-17 05:51:46 +0900

    [FEAT/#43] 바텀시트 리스트 아이템 구현

---
+++ app/src/main/java/com/spoony/spoony/presentation/map/component/bottomsheet/MapListItem.kt
@@ -0,0 +1,95 @@
+package com.spoony.spoony.presentation.map.component.bottomsheet
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spoony.spoony.core.designsystem.component.image.UrlImage
+import com.spoony.spoony.core.designsystem.component.tag.IconTag
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun MapListItem(
+    placeName: String,
+    address: String,
+    review: String,
+    imageUrl: String,
+    categoryIconUrl: String,
+    categoryName: String,
+    textColor: String,
+    backgroundColor: String,
+    modifier: Modifier = Modifier
+) {
+    val typography = SpoonyAndroidTheme.typography
+    val color = SpoonyAndroidTheme.colors
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp, horizontal = 20.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 10.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = placeName,
+                    style = typography.body1b,
+                    color = color.black
+                )
+                IconTag(
+                    text = categoryName,
+                    iconUrl = categoryIconUrl,
+                    backgroundColorHex = backgroundColor,
+                    textColorHex = textColor,
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                )
+            }
+
+            Text(
+                text = address,
+                style = typography.caption1m,
+                color = color.gray600,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+            )
+
+            Text(
+                text = review,
+                style = typography.caption1m,
+                color = color.black,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .border(BorderStroke(1.dp, color.gray0))
+                    .padding(vertical = 12.dp, horizontal = 10.dp)
+
+            )
+        }
+
+        UrlImage(
+            imageUrl = imageUrl,
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .size(98.dp)
+        )
+    }
+}
