ad57dd3a556c9a0f6b4f37c4cfd3279c3a196f51
Hyobeen-Park <amanda0109@naver.com>
2025-01-17 06:47:37 +0900

    [FEAT/#43] 지도 상세리뷰 카드 구현

--- app/src/main/java/com/spoony/spoony/presentation/map/component/bottomsheet/MapListItem.kt
+++ app/src/main/java/com/spoony/spoony/presentation/map/component/bottomsheet/MapListItem.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.spoony.spoony.core.designsystem.component.image.UrlImage
 import com.spoony.spoony.core.designsystem.component.tag.IconTag
