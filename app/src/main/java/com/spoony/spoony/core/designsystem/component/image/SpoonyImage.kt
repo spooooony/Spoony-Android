2b063a8918a772a73687901df6b63a23fc4710e4
Hyobeen-Park <amanda0109@naver.com>
2025-01-17 04:43:44 +0900

    [CHORE/#52] Image 삭제 및 네이밍 수정

--- app/src/main/java/com/spoony/spoony/core/designsystem/component/image/SpoonyImage.kt
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/image/SpoonyImage.kt
@@ -1,7 +1,5 @@
 package com.spoony.spoony.core.designsystem.component.image
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -9,12 +7,11 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 
 @Composable
-fun SpoonyImage(
+fun UrlImage(
     imageUrl: String,
     modifier: Modifier = Modifier,
     shape: Shape = RectangleShape,
@@ -26,23 +23,6 @@ fun SpoonyImage(
             .data(imageUrl)
             .crossfade(true)
             .build(),
-        contentDescription = contentDescription,
-        contentScale = contentScale,
-        modifier = modifier
-            .clip(shape)
-    )
-}
-
-@Composable
-fun SpoonyImage(
-    @DrawableRes imageId: Int,
-    modifier: Modifier = Modifier,
-    shape: Shape = RectangleShape,
-    contentScale: ContentScale = ContentScale.Crop,
-    contentDescription: String? = null
-) {
-    Image(
-        painter = painterResource(imageId),
         contentDescription = contentDescription,
         contentScale = contentScale,
         modifier = modifier
