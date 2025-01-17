17a67f0f30bbac11a2a163eb15698515029a4ce7
roel <ash49901@gmail.com>
2025-01-18 05:40:42 +0900

    [FEAT/#60] ReportOption code 변수 추가

--- app/src/main/java/com/spoony/spoony/presentation/report/type/ReportOption.kt
+++ app/src/main/java/com/spoony/spoony/presentation/report/type/ReportOption.kt
@@ -1,10 +1,13 @@
 package com.spoony.spoony.presentation.report.type
 
-enum class ReportOption(val displayName: String) {
-    ADVERTISEMENT("영리 목적/ 홍보성 후기"),
-    INSULT("욕설/ 인신 공격"),
-    ILLEGAL_INFO("불법정보"),
-    PERSONAL_INFO("개인 정보 노출"),
-    DUPLICATE("도배"),
-    OTHER("기타")
+enum class ReportOption(
+    val displayName: String,
+    val code: String
+) {
+    ADVERTISEMENT("영리 목적/ 홍보성 후기", "ADVERTISEMENT"),
+    INSULT("욕설/ 인신 공격", "INSULT"),
+    ILLEGAL_INFO("불법정보", "ILLEGAL_INFO"),
+    PERSONAL_INFO("개인 정보 노출", "PERSONAL_INFO"),
+    DUPLICATE("도배", "DUPLICATE"),
+    OTHER("기타", "OTHER")
 }
