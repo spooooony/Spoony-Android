ee59f5f347580b0e49b106741b61b7946b2d58ff
roel <ash49901@gmail.com>
2025-01-17 22:19:50 +0900

    [FEAT/#60] ReportScreen 구조 변경 및 padding 수정

--- app/src/main/java/com/spoony/spoony/presentation/report/ReportRoute.kt
+++ app/src/main/java/com/spoony/spoony/presentation/report/ReportRoute.kt
@@ -1,13 +1,12 @@
 package com.spoony.spoony.presentation.report
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -15,6 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,12 +23,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -40,6 +40,7 @@ import com.spoony.spoony.core.designsystem.component.topappbar.TitleTopAppBar
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
 import com.spoony.spoony.core.designsystem.type.ButtonSize
 import com.spoony.spoony.core.designsystem.type.ButtonStyle
+import com.spoony.spoony.core.util.extension.addFocusCleaner
 import com.spoony.spoony.presentation.report.component.ReportRadioButton
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.immutableListOf
@@ -64,34 +65,29 @@ private fun ReportScreen(
     selectedOption: String,
     reportContext: String
 ) {
+    val focusManager = LocalFocusManager.current
     var selectedOption by remember { mutableStateOf(selectedOption) }
     var reportContext by remember { mutableStateOf(reportContext) }
-    Box(
+    Column(
         modifier = Modifier
-            .fillMaxSize()
+            .addFocusCleaner(focusManager)
             .navigationBarsPadding()
             .statusBarsPadding()
     ) {
         TitleTopAppBar(
             title = "신고하기",
-            onBackButtonClick = {},
-            modifier = Modifier.drawBehind {
-                val strokeWidth = 2.dp.toPx()
-                val y = size.height - strokeWidth / 2
-                drawLine(
-                    color = Color.Gray,
-                    start = Offset(0f, y),
-                    end = Offset(size.width, y),
-                    strokeWidth = strokeWidth
-                )
-            }
+            onBackButtonClick = {}
+        )
+        HorizontalDivider(
+            thickness = Dp.Hairline,
+            color = SpoonyAndroidTheme.colors.gray100
         )
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 64.dp)
+                .fillMaxWidth()
                 .background(color = SpoonyAndroidTheme.colors.white)
-                .padding(start = 20.dp, end = 20.dp)
+                .imePadding()
+                .padding(horizontal = 20.dp)
                 .verticalScroll(rememberScrollState())
         ) {
             Spacer(modifier = Modifier.height(31.dp))
