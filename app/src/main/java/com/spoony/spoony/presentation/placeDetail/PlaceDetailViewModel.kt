08709489d23f553c76b2bec9d799d03051c6c6ed
roel <ash49901@gmail.com>
2025-01-18 01:44:24 +0900

    [FEAT/#9] MutableStateFlow 형태 변경

--- app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailViewModel.kt
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailViewModel.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 @HiltViewModel
 class PlaceDetailViewModel @Inject constructor() : ViewModel() {
-    var state: MutableStateFlow<PlaceDetailState> =
-        MutableStateFlow(PlaceDetailState())
-        private set
+    var _state: MutableStateFlow<PlaceDetailState> = MutableStateFlow(PlaceDetailState())
+    val state: StateFlow<PlaceDetailState>
+        get() = _state
 }
