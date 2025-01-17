49ba9f4ba540fbdde12964caf9432a891fdcfefd
roel <ash49901@gmail.com>
2025-01-18 05:21:59 +0900

    [FEAT/#60] ReportOption enum 수정

--- app/src/main/java/com/spoony/spoony/presentation/report/ReportOption.kt
+++ app/src/main/java/com/spoony/spoony/presentation/report/ReportOption.kt
@@ -1,10 +1,10 @@
 package com.spoony.spoony.presentation.report
 
 enum class ReportOption(val displayName: String) {
-    COMMERCIAL("영리 목적/ 홍보성 후기"),
-    ABUSIVE("욕설/ 인신 공격"),
-    ILLEGAL("불법정보"),
+    ADVERTISEMENT("영리 목적/ 홍보성 후기"),
+    INSULT("욕설/ 인신 공격"),
+    ILLEGAL_INFO("불법정보"),
     PERSONAL_INFO("개인 정보 노출"),
-    SPAM("도배"),
+    DUPLICATE("도배"),
     OTHER("기타")
 }
