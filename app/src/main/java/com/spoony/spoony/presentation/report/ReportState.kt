ed80f9b99b2f01b3e55d7446712a19037bb85fe6
roel <ash49901@gmail.com>
2025-01-18 10:49:19 +0900

    [FEAT/#60] var -> val 으로 수정

--- app/src/main/java/com/spoony/spoony/presentation/report/ReportState.kt
+++ app/src/main/java/com/spoony/spoony/presentation/report/ReportState.kt
@@ -6,6 +6,6 @@ import kotlinx.collections.immutable.toImmutableList
 
 data class ReportState(
     val reportOptions: ImmutableList<ReportOption> = ReportOption.entries.toImmutableList(),
-    var selectedReportOption: ReportOption = ReportOption.ADVERTISEMENT,
+    val selectedReportOption: ReportOption = ReportOption.ADVERTISEMENT,
     val reportContext: String = ""
 )
