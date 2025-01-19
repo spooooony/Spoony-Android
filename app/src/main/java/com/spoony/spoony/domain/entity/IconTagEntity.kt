3ba3653d5356e9aee8cee36f0049a7075376a6a0
roel <ash49901@gmail.com>
2025-01-20 04:49:12 +0900

    [FEAT/#9] IconTagEntity 기본값 제거

--- app/src/main/java/com/spoony/spoony/domain/entity/IconTagEntity.kt
+++ app/src/main/java/com/spoony/spoony/domain/entity/IconTagEntity.kt
@@ -1,8 +1,8 @@
 package com.spoony.spoony.domain.entity
 
 data class IconTagEntity(
-    val name: String = "",
-    val backgroundColorHex: String = "",
-    val textColorHex: String = "",
-    val iconUrl: String = ""
+    val name: String,
+    val backgroundColorHex: String,
+    val textColorHex: String,
+    val iconUrl: String
 )
