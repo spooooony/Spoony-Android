834d6c7215dc4f89f230d98f600b413fadb6c2ec
roel <ash49901@gmail.com>
2025-01-20 02:59:11 +0900

    [FEAT/#9] PlaceDetail state, viewModel 수정

--- app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailState.kt
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailState.kt
@@ -1,24 +1,12 @@
 package com.spoony.spoony.presentation.placeDetail
 
 import com.spoony.spoony.core.state.UiState
-import com.spoony.spoony.domain.entity.IconTagEntity
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.immutableListOf
+import com.spoony.spoony.domain.entity.PostEntity
+import com.spoony.spoony.domain.entity.UserEntity
 
 data class PlaceDetailState(
-    var iconTag: UiState<IconTagEntity> = UiState.Loading,
-    var textTitle: String = "",
-    var textContent: String = "",
-    var profileUrl: String = "",
-    var profileName: String = "",
-    var profileLocation: String = "",
-    var imageList: ImmutableList<String> = immutableListOf(),
-    var dateString: String = "",
-    var locationAddress: String = "",
-    var locationName: String = "",
-    var locationPinCount: Int = 0,
-    var mySpoonCount: Int = 0,
-    var menuItems: ImmutableList<String> = immutableListOf(),
-    var isSpoonEat: Boolean = false,
-    var isMyMap: Boolean = false
+    val postEntity: UiState<PostEntity> = UiState.Loading,
+    val userEntity: UiState<UserEntity> = UiState.Loading,
+    val spoonAmountEntity: UiState<Int> = UiState.Loading,
+    val scoopDialogVisibility: Boolean = false
 )
--- app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailViewModel.kt
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailViewModel.kt
@@ -1,6 +1,8 @@
 package com.spoony.spoony.presentation.placeDetail
 
 import androidx.lifecycle.ViewModel
+import com.spoony.spoony.core.state.UiState
+import com.spoony.spoony.domain.entity.PostEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,4 +13,28 @@ class PlaceDetailViewModel @Inject constructor() : ViewModel() {
     var _state: MutableStateFlow<PlaceDetailState> = MutableStateFlow(PlaceDetailState())
     val state: StateFlow<PlaceDetailState>
         get() = _state
+
+    fun updateScoop(isScoop: Boolean) {
+        _state.value = _state.value.copy(
+            postEntity = when (_state.value.postEntity) {
+                is UiState.Success -> {
+                    val currentPost = (_state.value.postEntity as UiState.Success<PostEntity>).data
+                    UiState.Success(currentPost.copy(isScoop = !isScoop))
+                }
+                else -> _state.value.postEntity
+            }
+        )
+    }
+
+    fun updateZzim(isZzim: Boolean) {
+        _state.value = _state.value.copy(
+            postEntity = when (_state.value.postEntity) {
+                is UiState.Success -> {
+                    val currentPost = (_state.value.postEntity as UiState.Success<PostEntity>).data
+                    UiState.Success(currentPost.copy(isZzim = !isZzim))
+                }
+                else -> _state.value.postEntity
+            }
+        )
+    }
 }
