4306fd9f0bfdb60635a0470d84c76298fff8f7e1
roel <ash49901@gmail.com>
2025-01-21 00:40:43 +0900

    [FIX/#87] IconChip 구조 변경

--- app/src/main/java/com/spoony/spoony/core/designsystem/component/chip/IconChip.kt
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/chip/IconChip.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,22 +31,17 @@ import com.spoony.spoony.core.util.extension.noRippleClickable
 @Composable
 fun IconChip(
     text: String,
-    selectedTextColor: Color,
-    unSelectedTextColor: Color,
     selectedIconUrl: String,
     unSelectedIconUrl: String,
-    isSelected: Boolean,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+    isGradient: Boolean = false
 ) {
     val context = LocalContext.current
 
-    val iconUrl = if (isSelected) selectedIconUrl else unSelectedIconUrl
-
-    val textColor = if (isSelected) selectedTextColor else unSelectedTextColor
-
     Row(
-        modifier = Modifier
+        modifier = modifier
             .noRippleClickable(onClick)
             .clip(RoundedCornerShape(12.dp))
             .border(
@@ -56,7 +50,15 @@ fun IconChip(
                 shape = RoundedCornerShape(12.dp)
             )
             .then(
-                modifier
+                if (isSelected) {
+                    if (isGradient) {
+                        Modifier.background(color = SpoonyAndroidTheme.colors.black)
+                    } else {
+                        Modifier.background(color = SpoonyAndroidTheme.colors.main400)
+                    }
+                } else {
+                    Modifier.background(color = SpoonyAndroidTheme.colors.gray0)
+                }
             )
             .padding(
                 horizontal = 14.dp,
@@ -67,17 +69,19 @@ fun IconChip(
     ) {
         AsyncImage(
             model = ImageRequest.Builder(context)
-                .data(iconUrl)
+                .data(if (isSelected) selectedIconUrl else unSelectedIconUrl)
                 .crossfade(true)
                 .build(),
             modifier = Modifier.size(16.dp),
             contentScale = ContentScale.Crop,
             contentDescription = null
         )
+
         Spacer(modifier = Modifier.width(4.dp))
+
         Text(
             text = text,
-            color = textColor,
+            color = if (isSelected) SpoonyAndroidTheme.colors.white else SpoonyAndroidTheme.colors.gray600,
             style = SpoonyAndroidTheme.typography.body2sb
         )
     }
@@ -94,60 +98,23 @@ private fun IconChipPreview() {
         ) {
             IconChip(
                 text = "chip",
-                selectedTextColor = Color.Black,
-                unSelectedTextColor = Color.White,
-                modifier = Modifier.then(
-                    if (isSelected) {
-                        Modifier.background(Color.White)
-                    } else {
-                        Modifier.background(Color.Black)
-                    }
-                ),
                 onClick = {
                     isSelected = !isSelected
                 },
+                isGradient = true,
                 isSelected = isSelected,
-                selectedIconUrl = "https://github.com/user-attachments/assets/1e0bc0e5-a1c6-44e8-b6f4-f47aced44441",
-                unSelectedIconUrl = "https://github.com/user-attachments/assets/c1752fb8-c771-4931-8a45-3d43a76209f8"
-
+                unSelectedIconUrl = "https://github.com/user-attachments/assets/1e0bc0e5-a1c6-44e8-b6f4-f47aced44441",
+                selectedIconUrl = "https://github.com/user-attachments/assets/c1752fb8-c771-4931-8a45-3d43a76209f8"
             )
 
             IconChip(
                 text = "chip",
-                selectedTextColor = Color.White,
-                unSelectedTextColor = SpoonyAndroidTheme.colors.gray600,
-                modifier = Modifier.then(
-                    if (isSelected) {
-                        Modifier.background(Color.Black)
-                    } else {
-                        Modifier.background(SpoonyAndroidTheme.colors.gray0)
-                    }
-                ),
                 onClick = {
                     isSelected = !isSelected
                 },
                 isSelected = isSelected,
-                selectedIconUrl = "https://github.com/user-attachments/assets/c1752fb8-c771-4931-8a45-3d43a76209f8",
-                unSelectedIconUrl = "https://github.com/user-attachments/assets/1e0bc0e5-a1c6-44e8-b6f4-f47aced44441"
-            )
-
-            IconChip(
-                text = "chip",
-                selectedTextColor = Color.White,
-                unSelectedTextColor = Color.White,
-                onClick = {
-                    isSelected = !isSelected
-                },
-                modifier = Modifier.then(
-                    if (isSelected) {
-                        Modifier.background(Color.Black)
-                    } else {
-                        Modifier.background(SpoonyAndroidTheme.colors.main400)
-                    }
-                ),
-                isSelected = isSelected,
-                selectedIconUrl = "https://github.com/user-attachments/assets/c1752fb8-c771-4931-8a45-3d43a76209f8",
-                unSelectedIconUrl = "https://github.com/user-attachments/assets/c1752fb8-c771-4931-8a45-3d43a76209f8"
+                unSelectedIconUrl = "https://github.com/user-attachments/assets/1e0bc0e5-a1c6-44e8-b6f4-f47aced44441",
+                selectedIconUrl = "https://github.com/user-attachments/assets/c1752fb8-c771-4931-8a45-3d43a76209f8"
             )
         }
     }
