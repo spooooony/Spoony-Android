0ccb96a6ffdb1a7ffc5b9116db19a8ab8e6104bc
roel <ash49901@gmail.com>
2025-01-20 17:58:10 +0900

    [FEAT/#81] ktLintFormat 처리

--- app/src/main/java/com/spoony/spoony/presentation/placeDetail/component/ScoopDialog.kt
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/component/ScoopDialog.kt
@@ -1,19 +1,10 @@
 package com.spoony.spoony.presentation.placeDetail.component
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +17,7 @@ import com.spoony.spoony.core.designsystem.component.dialog.TwoButtonDialog
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
 
 @Composable
-fun ScoopDialog (
+fun ScoopDialog(
     onClickPositive: () -> Unit,
     onClickNegative: () -> Unit
 ) {
