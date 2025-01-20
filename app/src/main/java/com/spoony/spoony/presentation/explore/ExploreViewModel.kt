23fc3536a1357689947bca302716064078493e50
Hyobeen-Park <amanda0109@naver.com>
2025-01-20 16:45:35 +0900

    [CHORE/#82] 더미데이터 삭제

--- app/src/main/java/com/spoony/spoony/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/spoony/spoony/presentation/explore/ExploreViewModel.kt
@@ -1,13 +1,9 @@
 package com.spoony.spoony.presentation.explore
 
 import androidx.lifecycle.ViewModel
-import com.spoony.spoony.core.state.UiState
-import com.spoony.spoony.domain.entity.CategoryEntity
-import com.spoony.spoony.presentation.explore.model.FeedModel
 import com.spoony.spoony.presentation.explore.type.SortingOption
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -18,96 +14,6 @@ class ExploreViewModel @Inject constructor() : ViewModel() {
     val state: StateFlow<ExploreState>
         get() = _state
 
-    init {
-        _state.update {
-            it.copy(
-                feedList = UiState.Success(
-                    persistentListOf(
-                        FeedModel(
-                            feedId = 0,
-                            userId = 1,
-                            username = "효비",
-                            userRegion = "마포구",
-                            title = "안녕!!",
-                            categoryEntity = CategoryEntity(
-                                categoryId = 1,
-                                categoryName = "주류",
-                                iconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                unSelectedIconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                textColor = "000000",
-                                backgroundColor = "000000"
-                            ),
-                            addMapCount = 99
-                        ),
-                        FeedModel(
-                            feedId = 1,
-                            userId = 1,
-                            username = "효비",
-                            userRegion = "마포구",
-                            title = "안녕!!",
-                            categoryEntity = CategoryEntity(
-                                categoryId = 1,
-                                categoryName = "주류",
-                                iconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                unSelectedIconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                textColor = "000000",
-                                backgroundColor = "000000"
-                            ),
-                            addMapCount = 99
-                        ),
-                        FeedModel(
-                            feedId = 2,
-                            userId = 1,
-                            username = "효비",
-                            userRegion = "마포구",
-                            title = "안녕!!",
-                            categoryEntity = CategoryEntity(
-                                categoryId = 1,
-                                categoryName = "주류",
-                                iconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                unSelectedIconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                textColor = "000000",
-                                backgroundColor = "000000"
-                            ),
-                            addMapCount = 99
-                        ),
-                        FeedModel(
-                            feedId = 3,
-                            userId = 1,
-                            username = "효비",
-                            userRegion = "마포구",
-                            title = "안녕!!",
-                            categoryEntity = CategoryEntity(
-                                categoryId = 1,
-                                categoryName = "주류",
-                                iconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                unSelectedIconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                textColor = "000000",
-                                backgroundColor = "000000"
-                            ),
-                            addMapCount = 99
-                        ),
-                        FeedModel(
-                            feedId = 4,
-                            userId = 1,
-                            username = "효비",
-                            userRegion = "마포구",
-                            title = "안녕!!",
-                            categoryEntity = CategoryEntity(
-                                categoryId = 1,
-                                categoryName = "주류",
-                                iconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                unSelectedIconUrl = "https://avatars.githubusercontent.com/u/93641814?s=60&v=4",
-                                textColor = "000000",
-                                backgroundColor = "000000"
-                            ),
-                            addMapCount = 99
-                        )
-                    )
-                )
-            )
-        }
-    }
     fun updateSelectedSortingOption(sortingOption: SortingOption) {
         _state.update {
             it.copy(
