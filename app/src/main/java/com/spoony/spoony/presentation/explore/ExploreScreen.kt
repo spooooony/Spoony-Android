62c1220674d6cd5a55f1cd01b1497dc6980fcf8e
Hyobeen-Park <amanda0109@naver.com>
2025-01-20 12:12:11 +0900

    [FEAT/#82] 탑앱바 및 지역선택 바텀시트 연결

--- app/src/main/java/com/spoony/spoony/presentation/explore/ExploreScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/explore/ExploreScreen.kt
@@ -1,31 +1,50 @@
 package com.spoony.spoony.presentation.explore
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.spoony.spoony.presentation.explore.component.bottomsheet.ExploreSortingBottomSheet
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.spoony.spoony.presentation.explore.component.ExploreTopAppBar
+import com.spoony.spoony.presentation.explore.component.bottomsheet.ExploreLocationBottomSheet
 
 @Composable
 fun ExploreRoute(
     paddingValues: PaddingValues
 ) {
-    ExploreScreen()
+    ExploreScreen(
+        spoonCount = 99,
+        selectedCity = "마포구",
+        onLocationSortingButtonClick = {}
+    )
 }
 
 @Composable
-fun ExploreScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        ExploreSortingBottomSheet(
-            onDismiss = {},
-            onClick = {}
+fun ExploreScreen(
+    spoonCount: Int,
+    selectedCity: String,
+    onLocationSortingButtonClick: (String) -> Unit,
+) {
+    var isLocationBottomSheetVisible by remember { mutableStateOf(false) }
+
+    if (isLocationBottomSheetVisible) {
+        ExploreLocationBottomSheet(
+            selectedCity = selectedCity,
+            onDismiss = { isLocationBottomSheetVisible = false },
+            onClick = onLocationSortingButtonClick
         )
+    }
+
+    LazyColumn {
+        item {
+            ExploreTopAppBar(
+                count = spoonCount,
+                onClick = {
+                    isLocationBottomSheetVisible = true
+                },
+            )
+        }
     }
 }
