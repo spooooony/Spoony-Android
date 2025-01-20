19a92a6a03f54129aa63185b35f75d6a1312004f
Hyobeen-Park <amanda0109@naver.com>
2025-01-20 14:28:53 +0900

    [FEAT/#82] 탐색 정렬 버튼 구현 및 바텀시트 연결

--- app/src/main/java/com/spoony/spoony/presentation/explore/ExploreScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/explore/ExploreScreen.kt
@@ -2,20 +2,34 @@ package com.spoony.spoony.presentation.explore
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import com.spoony.spoony.R
 import com.spoony.spoony.core.designsystem.component.chip.IconChip
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
 import com.spoony.spoony.core.designsystem.type.ChipColor
+import com.spoony.spoony.core.util.extension.noRippleClickable
 import com.spoony.spoony.domain.entity.CategoryEntity
 import com.spoony.spoony.presentation.explore.component.ExploreTopAppBar
 import com.spoony.spoony.presentation.explore.component.bottomsheet.ExploreLocationBottomSheet
+import com.spoony.spoony.presentation.explore.component.bottomsheet.ExploreSortingBottomSheet
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -43,12 +57,21 @@ fun ExploreScreen(
     updateSelectedCategory: (Int) -> Unit
 ) {
     var isLocationBottomSheetVisible by remember { mutableStateOf(false) }
+    var isSortingBottomSheetVisible by remember { mutableStateOf(false) }
 
     if (isLocationBottomSheetVisible) {
         ExploreLocationBottomSheet(
             selectedCity = selectedCity,
             onDismiss = { isLocationBottomSheetVisible = false },
             onClick = onLocationSortingButtonClick
+        )
+    }
+
+    if (isSortingBottomSheetVisible) {
+        ExploreSortingBottomSheet(
+            onDismiss = { isSortingBottomSheetVisible = false },
+            onClick = { },
+            currentSortingOption = SortingOption.LATEST
         )
     }
 
@@ -75,6 +98,36 @@ fun ExploreScreen(
                         tagColor = if (selectedCategory == category.categoryId) ChipColor.Black else ChipColor.White,
                         iconUrl = category.iconUrl,
                         onClick = { updateSelectedCategory(category.categoryId) }
+                    )
+                }
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp, horizontal = 20.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .noRippleClickable { isSortingBottomSheetVisible = true }
+                        .padding(4.dp)
+                ) {
+                    Text(
+                        text = "최신순",
+                        style = SpoonyAndroidTheme.typography.caption1m,
+                        color = SpoonyAndroidTheme.colors.gray700,
+                        modifier = Modifier
+                            .padding(end = 2.dp)
+                    )
+
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_filter_24),
+                        contentDescription = null,
+                        tint = SpoonyAndroidTheme.colors.gray700,
+                        modifier = Modifier
+                            .size(16.dp)
                     )
                 }
             }
