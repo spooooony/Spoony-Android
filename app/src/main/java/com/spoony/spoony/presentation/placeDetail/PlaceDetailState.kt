83aa94460dc090a9b8b78cbfde4b56ccd3795399
roel <ash49901@gmail.com>
2025-01-21 02:17:12 +0900

    [FEAT/#9] persistentListOf -> immutableListOf 로 수정

--- app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailState.kt
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/PlaceDetailState.kt
@@ -4,12 +4,12 @@ import com.spoony.spoony.core.state.UiState
 import com.spoony.spoony.domain.entity.PostEntity
 import com.spoony.spoony.domain.entity.UserEntity
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.immutableListOf
+import kotlinx.collections.immutable.persistentListOf
 
 data class PlaceDetailState(
     val postEntity: UiState<PostEntity> = UiState.Loading,
     val userEntity: UiState<UserEntity> = UiState.Loading,
     val spoonAmountEntity: UiState<Int> = UiState.Loading,
     val scoopDialogVisibility: Boolean = false,
-    val dropDownMenuList: ImmutableList<String> = immutableListOf("신고하기")
+    val dropDownMenuList: ImmutableList<String> = persistentListOf("신고하기")
 )
