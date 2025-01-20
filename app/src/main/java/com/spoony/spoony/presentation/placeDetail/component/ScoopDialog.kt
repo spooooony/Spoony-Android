4fa52689ee1f927155a29f7b91047cbfa0a92260
roel <ash49901@gmail.com>
2025-01-20 17:55:17 +0900

    [FEAT/#81] ScoopDialog 떠먹기 다이얼로그 구현

---
+++ app/src/main/java/com/spoony/spoony/presentation/placeDetail/component/ScoopDialog.kt
@@ -0,0 +1,66 @@
+package com.spoony.spoony.presentation.placeDetail.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.spoony.spoony.R
+import com.spoony.spoony.core.designsystem.component.dialog.TwoButtonDialog
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun ScoopDialog (
+    onClickPositive: () -> Unit,
+    onClickNegative: () -> Unit
+) {
+    val lottieComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.spoony_detail_use))
+
+    TwoButtonDialog(
+        message = "수저 1개를 사용하여 떠먹어 볼까요?",
+        negativeText = "아니요",
+        onClickNegative = onClickNegative,
+        positiveText = "떠먹을래요",
+        onClickPositive = onClickPositive,
+        onDismiss = onClickNegative,
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .sizeIn(minHeight = 150.dp)
+            ) {
+                LottieAnimation(
+                    composition = lottieComposition,
+                    iterations = LottieConstants.IterateForever
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun ScoopDialogPreview() {
+    SpoonyAndroidTheme {
+        ScoopDialog(
+            onClickPositive = {},
+            onClickNegative = {}
+        )
+    }
+}
