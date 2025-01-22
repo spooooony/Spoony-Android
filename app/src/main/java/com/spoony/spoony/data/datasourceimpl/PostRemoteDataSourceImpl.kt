040a6578dca303804cdc75d930ba38d2eacbc60c
roel <ash49901@gmail.com>
2025-01-23 02:48:19 +0900

    [FEAT/#111] BaseResponseDTO -> BaseResponse 로 수정

--- app/src/main/java/com/spoony/spoony/data/datasourceimpl/PostRemoteDataSourceImpl.kt
+++ app/src/main/java/com/spoony/spoony/data/datasourceimpl/PostRemoteDataSourceImpl.kt
@@ -1,7 +1,7 @@
 package com.spoony.spoony.data.datasourceimpl
 
 import com.spoony.spoony.data.datasource.PostRemoteDataSource
-import com.spoony.spoony.data.dto.base.BaseResponseDTO
+import com.spoony.spoony.data.dto.base.BaseResponse
 import com.spoony.spoony.data.dto.response.GetPostResponseDTO
 import com.spoony.spoony.data.service.PostService
 import javax.inject.Inject
@@ -9,5 +9,5 @@ import javax.inject.Inject
 class PostRemoteDataSourceImpl @Inject constructor(
     private val postService: PostService
 ) : PostRemoteDataSource {
-    override suspend fun getPostData(postId: Int, userId: Int): BaseResponseDTO<GetPostResponseDTO> = postService.getPost(userId = userId, postId = postId)
+    override suspend fun getPostData(postId: Int, userId: Int): BaseResponse<GetPostResponseDTO> = postService.getPost(userId = userId, postId = postId)
 }
--- app/src/main/java/com/spoony/spoony/data/dto/base/BaseResponse.kt
+++ app/src/main/java/com/spoony/spoony/data/dto/base/BaseResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class BaseResponseDTO<T>(
+data class BaseResponse<T>(
     @SerialName("success")
     val success: Boolean,
     @SerialName("error")
