9e6c7c558fa0a0fb100078ac7c44b08e2e1fecac
MinJae Han <hanminjae131@gmail.com>
2025-01-22 22:08:05 +0900

    [FEAT/#112] minHeight 적용

--- app/src/main/java/com/spoony/spoony/presentation/register/RegisterStepTwoScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/register/RegisterStepTwoScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -128,7 +129,7 @@ fun RegisterStepTwoScreen(
             }
         ) {
             LottieAnimation(
-                modifier = Modifier,
+                modifier = Modifier.sizeIn(minHeight = 150.dp),
                 composition = lottieComposition,
                 iterations = LottieConstants.IterateForever
             )
