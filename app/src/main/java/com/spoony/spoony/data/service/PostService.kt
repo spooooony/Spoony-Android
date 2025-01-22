3a3ff75abeddfe337bdb2e9508f500e5cb2fea08
roel <ash49901@gmail.com>
2025-01-23 03:58:26 +0900

    [FEAT/#120] PostService companion object 제거

--- app/src/main/java/com/spoony/spoony/data/service/PostService.kt
+++ app/src/main/java/com/spoony/spoony/data/service/PostService.kt
@@ -6,13 +6,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface PostService {
-    companion object {
-        const val API = "api"
-        const val V1 = "v1"
-        const val POST = "post"
-    }
-
-    @POST("/$API/$V1/$POST/scoop")
+    @POST("/api/v1/post/scoop")
     suspend fun postScoopPost(
         @Body postScoopRequestDTO: PostScoopRequestDTO
     ): BaseResponse<Boolean>
