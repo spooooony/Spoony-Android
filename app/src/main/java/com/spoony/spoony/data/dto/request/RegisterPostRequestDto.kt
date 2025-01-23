04bb3c7fea86a10f1da3348c73a90d5a6a94c6d8
MinJae Han <hanminjae131@gmail.com>
2025-01-23 22:25:39 +0900

    [MOD/#134] 리뷰 사항 반영

--- app/src/main/java/com/spoony/spoony/data/dto/request/RegisterPostRequestDto.kt
+++ app/src/main/java/com/spoony/spoony/data/dto/request/RegisterPostRequestDto.kt
@@ -3,17 +3,26 @@ package com.spoony.spoony.data.dto.request
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// 문제 없음.
 @Serializable
 data class RegisterPostRequestDto(
-    @SerialName("userId") val userId: Long,
-    @SerialName("title") val title: String,
-    @SerialName("description") val description: String,
-    @SerialName("placeName") val placeName: String,
-    @SerialName("placeAddress") val placeAddress: String,
-    @SerialName("placeRoadAddress") val placeRoadAddress: String,
-    @SerialName("latitude") val latitude: Double,
-    @SerialName("longitude") val longitude: Double,
-    @SerialName("categoryId") val categoryId: Int,
-    @SerialName("menuList") val menuList: List<String>
+    @SerialName("userId")
+    val userId: Long,
+    @SerialName("title")
+    val title: String,
+    @SerialName("description")
+    val description: String,
+    @SerialName("placeName")
+    val placeName: String,
+    @SerialName("placeAddress")
+    val placeAddress: String,
+    @SerialName("placeRoadAddress")
+    val placeRoadAddress: String,
+    @SerialName("latitude")
+    val latitude: Double,
+    @SerialName("longitude")
+    val longitude: Double,
+    @SerialName("categoryId")
+    val categoryId: Int,
+    @SerialName("menuList")
+    val menuList: List<String>
 )
