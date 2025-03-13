e03fdcb0718f26a4f128ee82ab6e9932b9b6bdb2
Hyobeen-Park <amanda0109@naver.com>
2025-03-13 15:05:47 +0900

    [FEAT/#46] BasicTopAppBar를 CenterAlignedTopAppBar 사용하지 않고 구현

--- app/src/main/java/com/spoony/spoony/core/designsystem/component/topappbar/SpoonyBasicTopAppBar.kt
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/topappbar/SpoonyBasicTopAppBar.kt
@@ -1,13 +1,15 @@
 package com.spoony.spoony.core.designsystem.component.topappbar
 
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -16,40 +18,46 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.spoony.spoony.R
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+import com.spoony.spoony.core.util.extension.noRippleClickable
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SpoonyBasicTopAppBar(
     modifier: Modifier = Modifier,
     showBackButton: Boolean = false,
     backgroundColor: Color = SpoonyAndroidTheme.colors.white,
     onBackButtonClick: () -> Unit = {},
-    actions: @Composable RowScope.() -> Unit = {},
+    actions: @Composable () -> Unit = {},
     content: @Composable () -> Unit = {}
 ) {
-    CenterAlignedTopAppBar(
-        title = content,
-        modifier = modifier,
-        navigationIcon = {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .background(backgroundColor)
+            .fillMaxWidth()
+            .padding(vertical = 12.dp)
+            .statusBarsPadding()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .weight(1f)
+        ) {
             if (showBackButton) {
-                IconButton(
-                    onClick = onBackButtonClick
-                ) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_left_24),
-                        contentDescription = null,
-                        tint = SpoonyAndroidTheme.colors.gray700,
-                        modifier = Modifier
-                            .size(32.dp)
-                    )
-                }
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_left_24),
+                    contentDescription = null,
+                    tint = SpoonyAndroidTheme.colors.gray700,
+                    modifier = Modifier
+                        .size(32.dp)
+                        .noRippleClickable(onBackButtonClick)
+                        .padding(start = 12.dp)
+                )
             }
-        },
-        actions = actions,
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = backgroundColor
-        )
-    )
+            content()
+        }
+        actions()
+    }
 }
 
 @Preview
