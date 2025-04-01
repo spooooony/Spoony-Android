9166c4146172ba77a84027f7f4ee3bbf6214a38e
MinJae Han <hanminjae131@gmail.com>
2025-04-02 00:52:11 +0900

    [MOD/#225] RegisterTooltip 화살표 위치 조정
    RegisterTooltip의 화살표 위치를 `0.632f`로 조정하여 시각적 배치를 개선합니다.

--- app/src/main/java/com/spoony/spoony/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/register/RegisterScreen.kt
@@ -94,7 +94,8 @@ fun RegisterScreen(
                 .align(Alignment.BottomCenter)
         ) {
             RegisterTooltip(
-                text = "장소를 등록하면 수저를 획득할 수 있어요"
+                text = "장소를 등록하면 수저를 획득할 수 있어요",
+                arrowPositionFraction = 0.632f
             )
             LaunchedEffect(Unit) {
                 delay(SHOW_REGISTER_SNACKBAR_TIME)
