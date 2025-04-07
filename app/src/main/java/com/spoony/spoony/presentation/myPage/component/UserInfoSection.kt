799f563ac9a74e661d76bc967e7bab004f3b01c6
MinJae Han <hanminjae131@gmail.com>
2025-04-08 01:56:12 +0900

    [FEAT/#222] `UserInfoSection` 컴포넌트 추가
    - 사용자 프로필 이미지, 리뷰 수, 팔로워 수, 팔로잉 수를 표시하는 `UserInfoSection` 컴포넌트를 추가합니다.
    - 이미지 URL을 기반으로 이미지를 표시하는 `UrlImage` 컴포넌트를 사용합니다.
    - `NameAndCount` 컴포넌트를 생성하여 각 정보 항목(리뷰, 팔로워, 팔로잉)의 제목과 개수를 표시합니다.
    - 컴포넌트의 레이아웃, 스타일, 간격 등을 설정합니다.
    - 미리보기(`Preview`)를 추가하여 UI를 확인할 수 있도록 합니다.

---
+++ app/src/main/java/com/spoony/spoony/presentation/myPage/component/UserInfoSection.kt
@@ -0,0 +1,87 @@
+package com.spoony.spoony.presentation.myPage.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spoony.spoony.core.designsystem.component.image.UrlImage
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun UserInfoSection(
+    imageUrl: String,
+    reviewCount: Int,
+    followerCount: Int,
+    followingCount: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth().background(Color.White)
+    ) {
+        UrlImage(
+            imageUrl = imageUrl,
+            shape = CircleShape,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(85.dp)
+        )
+
+        NameAndCount(title = "리뷰", count = reviewCount.toString())
+        NameAndCount(title = "팔로워", count = followerCount.toString())
+        NameAndCount(title = "팔로잉", count = followingCount.toString())
+    }
+}
+
+@Composable
+private fun NameAndCount(
+    title: String,
+    count: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            style = SpoonyAndroidTheme.typography.caption1b,
+            color = SpoonyAndroidTheme.colors.gray400,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = count,
+            style = SpoonyAndroidTheme.typography.body1sb,
+            color = SpoonyAndroidTheme.colors.black,
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun NameAndCountPreview() {
+    SpoonyAndroidTheme {
+        UserInfoSection(
+            imageUrl = "https://example.com/image.jpg",
+            reviewCount = 100,
+            followerCount = 500,
+            followingCount = 200,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        )
+    }
+}
