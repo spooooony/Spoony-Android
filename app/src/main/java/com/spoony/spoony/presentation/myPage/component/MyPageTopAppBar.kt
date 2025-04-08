d87c866b46e7684bd27a939799827e9bbaebb83f
MinJae Han <hanminjae131@gmail.com>
2025-04-08 22:04:49 +0900

    [MOD/#222] `LogoTag`에 `onClick` 추가
    `LogoTag` 컴포저블에 클릭 시 동작을 위한 `onClick` 파라미터를 추가합니다.

--- app/src/main/java/com/spoony/spoony/presentation/myPage/component/MyPageTopAppBar.kt
+++ app/src/main/java/com/spoony/spoony/presentation/myPage/component/MyPageTopAppBar.kt
@@ -1,9 +1,13 @@
 package com.spoony.spoony.presentation.myPage.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -11,11 +15,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.spoony.spoony.R
 import com.spoony.spoony.core.designsystem.component.tag.LogoTag
-import com.spoony.spoony.core.designsystem.component.topappbar.SpoonyBasicTopAppBar
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
 import com.spoony.spoony.core.designsystem.theme.white
 import com.spoony.spoony.core.designsystem.type.TagSize
-import com.spoony.spoony.core.util.extension.noRippleClickable
 import com.spoony.spoony.core.util.extension.rotateClick
 
 @Composable
@@ -25,24 +27,27 @@ fun MyPageTopAppBar(
     onIconClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    SpoonyBasicTopAppBar(
-        content = {
-            LogoTag(
-                count = spoonCount,
-                tagSize = TagSize.Small,
-                modifier = Modifier.rotateClick(onClick = onLogoClick)
-            )
-        },
-        actions = {
-            Icon(
-                imageVector = ImageVector.vectorResource(id = R.drawable.ic_setting_24),
-                contentDescription = null,
-                tint = SpoonyAndroidTheme.colors.gray500,
-                modifier = Modifier.noRippleClickable(onClick = onIconClick)
-            )
-        },
-        modifier = modifier.background(white).padding(vertical = 2.dp)
-    )
+    Row(
+        modifier = modifier
+            .background(white)
+            .fillMaxWidth()
+            .padding(vertical = 14.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        LogoTag(
+            count = spoonCount,
+            tagSize = TagSize.Small,
+            onClick = onLogoClick
+        )
+
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_setting_24),
+            contentDescription = null,
+            tint = SpoonyAndroidTheme.colors.gray500,
+            modifier = Modifier.rotateClick(onClick = onIconClick)
+        )
+    }
 }
 
 @Preview
