3305c9fe9f5a119336a21331e86f9c358700ba09
MinJae Han <hanminjae131@gmail.com>
2025-04-09 02:33:22 +0900

    [FEAT/#237] Add `BackAndMenuTopAppBar` component
    - Creates a new `BackAndMenuTopAppBar` composable.
    - Includes back button functionality and optional kebab menu.
    - Uses `Icon` composable and vector resources for icons.
    - Implements `noRippleClickable` for button interactions.
    - Includes preview for the composable.
    - The app bar provides back and menu functionality.

---
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/topappbar/BackAndMenuTopAppBar.kt
@@ -0,0 +1,68 @@
+package com.spoony.spoony.core.designsystem.component.topappbar
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spoony.spoony.R
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+import com.spoony.spoony.core.util.extension.noRippleClickable
+
+@Composable
+fun BackAndMenuTopAppBar(
+    onBackButtonClick: () -> Unit,
+    onMenuButtonClick: () -> Unit,
+    wannaKebab: Boolean = false,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp, horizontal = 20.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(32.dp)
+                .noRippleClickable(onBackButtonClick)
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_left_24),
+                contentDescription = null,
+                tint = SpoonyAndroidTheme.colors.gray700
+            )
+        }
+
+        if (wannaKebab) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_kebabmenu_gray500_24),
+                contentDescription = null,
+                tint = SpoonyAndroidTheme.colors.gray500,
+                modifier = Modifier.noRippleClickable(onClick = onMenuButtonClick)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BackAndMenuTopAppBarPreview() {
+    SpoonyAndroidTheme {
+        BackAndMenuTopAppBar(
+            onBackButtonClick = {},
+            onMenuButtonClick = {}
+        )
+    }
+}
