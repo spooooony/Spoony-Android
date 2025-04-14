315a25277d1a8b1087db07050b8ab3f815bda005
MinJae Han <hanminjae131@gmail.com>
2025-04-14 17:30:23 +0900

    [MOD/#237] 린트 수정

--- app/src/main/java/com/spoony/spoony/presentation/follow/FollowViewModel.kt
+++ app/src/main/java/com/spoony/spoony/presentation/follow/FollowViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
-import com.spoony.spoony.domain.repository.UserRepository
 import com.spoony.spoony.presentation.follow.model.UserItemUiState
 import com.spoony.spoony.presentation.follow.navigation.Follow
 import dagger.hilt.android.lifecycle.HiltViewModel
