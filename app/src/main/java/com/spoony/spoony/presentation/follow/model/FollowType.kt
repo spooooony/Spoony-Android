45393c84736f90a9439e48d6c1684188f5ee611d
MinJae Han <hanminjae131@gmail.com>
2025-04-16 20:59:53 +0900

    [MOD/#237] Follow Route 수정

--- app/src/main/java/com/spoony/spoony/presentation/follow/model/FollowType.kt
+++ app/src/main/java/com/spoony/spoony/presentation/follow/model/FollowType.kt
@@ -3,11 +3,10 @@ package com.spoony.spoony.presentation.follow.model
 enum class FollowType {
     FOLLOWER,
     FOLLOWING;
-    
+
     fun toPageIndex(): Int = ordinal
 
     companion object {
         fun fromPageIndex(index: Int): FollowType = entries[index.mod(entries.size)]
     }
-
 }
