69510bacf8133d0ab321e463b2da31ecf47cce94
roel <ash49901@gmail.com>
2025-04-16 13:16:24 +0900

    [FEAT/#224] ExploreBottomSheet 파일 생성

---
+++ app/src/main/java/com/spoony/spoony/presentation/explore/component/bottomsheet/ExploreBottomSheet.kt
@@ -0,0 +1,26 @@
+package com.spoony.spoony.presentation.explore.component.bottomsheet
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+
+@Composable
+fun ExploreBottomSheet (
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDragHandle: () -> Unit,
+) {
+
+}
+
+@Preview
+@Composable
+private fun PreviewExploreBottomSheet() {
+    SpoonyAndroidTheme {
+        ExploreBottomSheet(
+            onDismissRequest = { },
+            onDragHandle = { },
+        )
+    }
+}
