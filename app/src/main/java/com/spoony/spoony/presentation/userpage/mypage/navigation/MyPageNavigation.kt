7be8af9a70772521d4a5b8defec2eab89552925b
MinJae Han <hanminjae131@gmail.com>
2025-04-17 21:13:10 +0900

    [MOD/#241] 타유저 페이지 구현

--- app/src/main/java/com/spoony/spoony/presentation/userpage/mypage/navigation/MyPageNavigation.kt
+++ app/src/main/java/com/spoony/spoony/presentation/userpage/mypage/navigation/MyPageNavigation.kt
@@ -1,7 +1,5 @@
 package com.spoony.spoony.presentation.userpage.mypage.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -9,9 +7,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.spoony.spoony.core.navigation.MainTabRoute
 import com.spoony.spoony.presentation.follow.model.FollowType
-import com.spoony.spoony.presentation.follow.navigation.shouldApplyFollowPageExitAnimation
 import com.spoony.spoony.presentation.userpage.mypage.MyPageRoute
-import com.spoony.spoony.presentation.userpage.otherpage.navigation.LONG_ANIMATION_DURATION
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToMyPage(
@@ -28,15 +24,11 @@ fun NavGraphBuilder.myPageNavGraph(
     navigateToRegister: () -> Unit,
     navigateToReviewDetail: (Int) -> Unit
 ) {
-    composable(route = MyPage.toString()) {
+    composable<MyPage> {
         MyPageRoute(
             paddingValues = paddingValues,
             navigateToSettings = navigateToSettings,
-            navigateToFollow = { followType, userId ->
-                // 마이페이지에서 팔로우 페이지로 이동할 때 Exit 애니메이션 활성화
-                shouldApplyFollowPageExitAnimation = true
-                navigateToFollow(followType, userId)
-            },
+            navigateToFollow = navigateToFollow,
             navigateToProfileEdit = navigateToProfileEdit,
             navigateToRegister = navigateToRegister,
             navigateToReviewDetail = navigateToReviewDetail
