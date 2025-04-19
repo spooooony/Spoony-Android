955951bf3342208684b455e379b6ffc9fe053c5c
MinJae Han <hanminjae131@gmail.com>
2025-04-19 16:34:45 +0900

    [MOD/#241] Refactor StateFlow handling and UserPageState
    -   Use `asStateFlow()` to expose `StateFlow` instances in `MyPageViewModel` and `OtherPageViewModel`, ensuring read-only access to the state.
    -   Simplify `UserPageState` by removing redundant `toUserPageState` functions and fields specific to MyPage or OtherPage.
    -   Update `FollowRoute` to properly use `onUserClick` lambda in `UserListScreen` for user interaction.
    -   Adjust the size of the checkbox in `LocalCheckBox` and Add padding.
    -   Simplify `UserType` enum.

--- app/src/main/java/com/spoony/spoony/presentation/userpage/component/LocalCheckBox.kt
+++ app/src/main/java/com/spoony/spoony/presentation/userpage/component/LocalCheckBox.kt
@@ -3,7 +3,6 @@ package com.spoony.spoony.presentation.userpage.component
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
