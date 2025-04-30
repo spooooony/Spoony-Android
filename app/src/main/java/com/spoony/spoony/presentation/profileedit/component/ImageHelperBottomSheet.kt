7eba60c396e35d2a180344e5bab31cfe92c6d088
MinJae Han <hanminjae131@gmail.com>
2025-04-30 11:11:33 +0900

    [MOD/#246] 프리뷰 제거

--- app/src/main/java/com/spoony/spoony/presentation/profileedit/component/ImageHelperBottomSheet.kt
+++ app/src/main/java/com/spoony/spoony/presentation/profileedit/component/ImageHelperBottomSheet.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.spoony.spoony.core.designsystem.component.bottomsheet.SpoonyBasicBottomSheet
 import kotlinx.collections.immutable.ImmutableList
