8ad1c5569681336ede46f0be836ea72312982b1f
Hyobeen-Park <amanda0109@naver.com>
2025-05-01 03:03:25 +0900

    [FEAT/#243] 닉네임 텍스트필드 구현

---
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/textfield/SpoonyNicknameTextField.kt
@@ -0,0 +1,200 @@
+package com.spoony.spoony.core.designsystem.component.textfield
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chattymin.pebble.containsEmoji
+import com.spoony.spoony.R
+import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
+import com.spoony.spoony.core.util.extension.addFocusCleaner
+import com.spoony.spoony.core.util.extension.noRippleClickable
+
+enum class NicknameTextFieldState(val text: String) {
+    DEFAULT(""),
+    AVAILABLE("사용 가능한 닉네임이에요"),
+    DUPLICATE("이미 사용 중인 닉네임이에요"),
+    INVALID_CHARACTERS("닉네임은 한글,영어,숫자만 사용할 수 있어요"),
+    OVERFLOW_MAX_LENGTH("10자 이하로 입력해주세요"),
+    NICKNAME_REQUIRED("닉네임은 필수예요")
+}
+
+@Composable
+fun SpoonyNicknameTextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    placeholder: String,
+    onDoneAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    maxLength: Int = 10,
+    minLength: Int = 1,
+    state: NicknameTextFieldState = NicknameTextFieldState.DEFAULT
+) {
+    var currentState by remember { mutableStateOf(state) }
+    var isFirstEntry by remember { mutableStateOf(true) }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+    val spoonyColors = SpoonyAndroidTheme.colors
+    val counterText = stringResource(R.string.COUNTER_TEXT, value.length, maxLength)
+
+    val borderColor = remember(currentState) {
+        when (currentState) {
+            NicknameTextFieldState.DEFAULT -> spoonyColors.gray100
+            NicknameTextFieldState.AVAILABLE -> spoonyColors.green400
+            else -> spoonyColors.error400
+        }
+    }
+
+    val textColor = remember(currentState) {
+        when (currentState) {
+            NicknameTextFieldState.DEFAULT -> spoonyColors.gray500
+            NicknameTextFieldState.AVAILABLE -> spoonyColors.green400
+            else -> spoonyColors.error400
+        }
+    }
+
+    LaunchedEffect(state) {
+        currentState = state
+    }
+
+    Column(
+        modifier = modifier
+            .addFocusCleaner(focusManager)
+    ) {
+        SpoonyBasicTextField(
+            value = value,
+            onValueChanged = { newText ->
+                when {
+                    newText.containsEmoji() || !SpoonyValidator.isNotContainsSpecialChars(newText) -> {
+                        currentState = NicknameTextFieldState.INVALID_CHARACTERS
+                    }
+
+                    newText.length > maxLength -> {
+                        currentState = NicknameTextFieldState.OVERFLOW_MAX_LENGTH
+                    }
+
+                    newText.trim().length < minLength -> {
+                        currentState = NicknameTextFieldState.NICKNAME_REQUIRED
+                        onValueChanged(newText)
+                    }
+
+                    else -> {
+                        currentState = NicknameTextFieldState.DEFAULT
+                        onValueChanged(newText)
+                    }
+                }
+            },
+            placeholder = placeholder,
+            borderColor = borderColor,
+            imeAction = ImeAction.Done,
+            onFocusChanged = {
+                if (isFirstEntry) {
+                    currentState = NicknameTextFieldState.DEFAULT
+                    isFirstEntry = false
+                } else if (value.trim().length < minLength) {
+                    currentState = NicknameTextFieldState.NICKNAME_REQUIRED
+                } else {
+                    onDoneAction()
+                }
+            },
+            onDoneAction = {
+                keyboardController?.hide()
+                focusManager.clearFocus()
+                if (currentState != NicknameTextFieldState.NICKNAME_REQUIRED) onDoneAction()
+            },
+            singleLine = true,
+            trailingIcon = {
+                if (value.isNotEmpty()) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_delete_filled_24),
+                        contentDescription = null,
+                        tint = spoonyColors.gray400,
+                        modifier = Modifier
+                            .noRippleClickable {
+                                onValueChanged("")
+                                currentState = NicknameTextFieldState.NICKNAME_REQUIRED
+                            }
+                    )
+                }
+            },
+            isAllowEmoji = true,
+            isAllowSpecialChars = true,
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            modifier = Modifier
+                .padding(top = 8.dp)
+        ) {
+            if (currentState != NicknameTextFieldState.DEFAULT) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(
+                        id = if (currentState == NicknameTextFieldState.AVAILABLE) R.drawable.ic_check_16 else R.drawable.ic_error_24
+                    ),
+                    contentDescription = null,
+                    tint = textColor,
+                    modifier = Modifier.size(16.dp)
+                )
+            }
+
+            Text(
+                text = currentState.text,
+                style = SpoonyAndroidTheme.typography.caption1m,
+                color = textColor,
+                modifier = Modifier
+                    .weight(1f)
+            )
+
+            Text(
+                text = counterText,
+                style = SpoonyAndroidTheme.typography.caption1m,
+                color = textColor
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SpoonyNicknameTextFieldPreview() {
+    SpoonyAndroidTheme {
+        var text by remember { mutableStateOf("") }
+        var state by remember { mutableStateOf(NicknameTextFieldState.DEFAULT) }
+
+        SpoonyNicknameTextField(
+            value = text,
+            onValueChanged = {
+                text = it
+                state = NicknameTextFieldState.DEFAULT
+            },
+            placeholder = "플레이스홀더",
+            onDoneAction = { state = NicknameTextFieldState.DUPLICATE },
+            maxLength = 10,
+            minLength = 1,
+            state = state,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
+}
