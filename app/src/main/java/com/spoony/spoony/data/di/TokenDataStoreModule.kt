09f802e7f39f296882aff8d8cc85c25b3f838199
Hyobeen-Park <amanda0109@naver.com>
2025-05-03 23:51:10 +0900

    [FEAT/#239] token dataStore 구현

---
+++ app/src/main/java/com/spoony/spoony/data/di/TokenDataStoreModule.kt
@@ -0,0 +1,40 @@
+package com.spoony.spoony.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.spoony.spoony.data.datasource.local.TokenDataSource
+import com.spoony.spoony.data.datasourceimpl.local.TokenDataSourceImpl
+import com.spoony.spoony.data.repositoryimpl.TokenRepositoryImpl
+import com.spoony.spoony.domain.repository.TokenRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private val Context.tokenDataStore: DataStore<Preferences> by preferencesDataStore(name = "token_preferences")
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TokenDataStoreModule {
+    @Provides
+    @Singleton
+    fun provideTokenDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = context.tokenDataStore
+
+    @Provides
+    @Singleton
+    fun provideTokenDataSource(
+        dataStore: DataStore<Preferences>
+    ): TokenDataSource = TokenDataSourceImpl(dataStore)
+
+    @Provides
+    @Singleton
+    fun provideTokenRepository(
+        dataSource: TokenDataSource
+    ): TokenRepository = TokenRepositoryImpl(dataSource)
+}
