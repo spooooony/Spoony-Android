8fda861aee33bd0ec9a72fe08f64a524519854b1
roel <ash49901@gmail.com>
2025-05-10 00:57:25 +0900

    [FEAT/#271] String.formatToYearMonthDay 수정

--- app/src/main/java/com/spoony/spoony/core/util/extension/StringExt.kt
+++ app/src/main/java/com/spoony/spoony/core/util/extension/StringExt.kt
@@ -8,15 +8,15 @@ import java.util.Locale
 fun String?.toValidHexColor(): String =
     if (this != null && Regex("^[0-9A-Fa-f]{6}$").matches(this)) this else "FFFFFF"
 
-fun String.formatToYearMonthDay(): String? {
+fun String.formatToYearMonthDay(): String {
     return try {
         val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS", Locale.getDefault())
         val outputFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일", Locale.getDefault())
         val dateTime = LocalDateTime.parse(this, inputFormatter)
         dateTime.format(outputFormatter)
     } catch (e: DateTimeParseException) {
-        null
+        ""
     } catch (e: Exception) {
-        null
+        ""
     }
 }
