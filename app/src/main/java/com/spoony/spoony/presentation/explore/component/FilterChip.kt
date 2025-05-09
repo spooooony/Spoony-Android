f6d26a8ac558b7f6f230602496c9c2d5ca4c3702
roel <ash49901@gmail.com>
2025-05-10 01:32:59 +0900

    [FEAT/#224] derivedStateOf 제거

--- app/src/main/java/com/spoony/spoony/presentation/explore/component/FilterChip.kt
+++ app/src/main/java/com/spoony/spoony/presentation/explore/component/FilterChip.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
