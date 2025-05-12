d2a81d3c65f5af86facd0aa096e00a71e212613f
Hyobeen-Park <amanda0109@naver.com>
2025-05-12 11:39:00 +0900

    [REFACTOR/#278] 탑앱바 리팩토링

--- app/src/main/java/com/spoony/spoony/presentation/gourmet/map/component/MapTopAppBar.kt
+++ app/src/main/java/com/spoony/spoony/presentation/gourmet/map/component/MapTopAppBar.kt
@@ -3,6 +3,8 @@ package com.spoony.spoony.presentation.gourmet.map.component
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,16 +13,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.spoony.spoony.R
-import com.spoony.spoony.core.designsystem.component.tag.LogoTag
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
-import com.spoony.spoony.core.designsystem.type.TagSize
 import com.spoony.spoony.core.util.extension.noRippleClickable
 
 @Composable
@@ -28,12 +27,12 @@ fun MapTopAppBar(
     onSearchClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Box(
         modifier = modifier
             .padding(vertical = 6.dp, horizontal = 20.dp)
     ) {
         Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier
                 .noRippleClickable(onClick = onSearchClick)
                 .background(
