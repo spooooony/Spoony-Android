33b85e0c73aa67d5e424cf319748d03ed4ae6e71
roel <ash49901@gmail.com>
2025-05-14 22:21:10 +0900

    [FEAT/#287] ExploreSearchUserModel 구현

---
+++ app/src/main/java/com/spoony/spoony/presentation/exploreSearch/model/ExploreSearchUserModel.kt
@@ -0,0 +1,17 @@
+package com.spoony.spoony.presentation.exploreSearch.model
+
+import com.spoony.spoony.domain.entity.UserEntity
+
+data class ExploreSearchUserModel(
+    val userId: Int,
+    val userName: String,
+    val userProfileUrl: String,
+    val userRegion: String
+)
+
+fun UserEntity.toModel(): ExploreSearchUserModel = ExploreSearchUserModel(
+    userId = this.userId,
+    userName = this.userName,
+    userProfileUrl = this.userProfileUrl,
+    userRegion = this.userRegion
+)
