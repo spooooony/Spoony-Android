857ca247135c570f48e45d921ee645b631c5a08d
roel <ash49901@gmail.com>
2025-05-14 22:20:28 +0900

    [FEAT/#287] 탐색 검색 유저 리스트 DTO 구현

---
+++ app/src/main/java/com/spoony/spoony/data/dto/response/UserListSearchResponseDto.kt
@@ -0,0 +1,22 @@
+package com.spoony.spoony.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserListSearchResponseDto(
+    @SerialName("userSimpleResponseDTO")
+    val userSimpleResponseDTO: List<UserSimpleResponseDto>
+)
+
+@Serializable
+data class UserSimpleResponseDto(
+    @SerialName("userId")
+    val userId: Int,
+    @SerialName("username")
+    val username: String,
+    @SerialName("regionName")
+    val regionName: String,
+    @SerialName("profileImageUrl")
+    val profileImageUrl: String
+)
