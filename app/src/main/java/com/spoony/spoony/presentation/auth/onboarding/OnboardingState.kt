805d8588e3c90e2b1e8741e5ef8b3eb23afd46c3
Hyobeen-Park <amanda0109@naver.com>
2025-05-17 15:32:48 +0900

    [FEAT/#275] OnboardingSteps에 step ordinal로 구현

--- app/src/main/java/com/spoony/spoony/presentation/auth/onboarding/OnboardingState.kt
+++ app/src/main/java/com/spoony/spoony/presentation/auth/onboarding/OnboardingState.kt
@@ -3,11 +3,14 @@ package com.spoony.spoony.presentation.auth.onboarding
 import com.spoony.spoony.core.designsystem.component.textfield.NicknameTextFieldState
 import com.spoony.spoony.core.designsystem.model.RegionModel
 
-enum class OnboardingSteps(val step: Float) {
-    ONE(1f),
-    TWO(2f),
-    THREE(3f),
-    END(0f)
+enum class OnboardingSteps() {
+    END,
+    ONE,
+    TWO,
+    THREE;
+
+    val step: Float
+        get() = this.ordinal.toFloat()
 }
 
 data class OnboardingState(
