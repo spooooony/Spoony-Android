f4fb42b08e57d49006cb737e8183154ac66c5507
roel <ash49901@gmail.com>
2025-05-23 03:14:39 +0900

    [FEAT/#287] Color.hexToColor 함수 수정

--- app/src/main/java/com/spoony/spoony/core/util/extension/ColorExt.kt
+++ app/src/main/java/com/spoony/spoony/core/util/extension/ColorExt.kt
@@ -3,15 +3,7 @@ package com.spoony.spoony.core.util.extension
 import android.graphics.Color.parseColor
 import androidx.compose.ui.graphics.Color
 
-fun Color.Companion.hexToColor(hex: String): Color {
-    return try {
-        if (hex.isEmpty()) {
-            Unspecified
-        } else {
-            val colorInt = parseColor("#$hex")
-            Color(colorInt)
-        }
-    } catch (e: Exception) {
-        Unspecified
-    }
+fun Color.Companion.hexToColor(hex: String?): Color {
+    val safeHex = hex.toValidHexColor()
+    return Color(parseColor("#$safeHex"))
 }
