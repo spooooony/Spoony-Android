199159cf7d5c6302ae9871f576790b8f0822f36b
Hyobeen-Park <amanda0109@naver.com>
2025-06-01 21:54:05 +0900

    [FEAT/#329] 지도 검색 후 필터링 칩 제거

--- app/src/main/java/com/spoony/spoony/presentation/gourmet/map/MapScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/gourmet/map/MapScreen.kt
@@ -462,28 +462,30 @@ private fun MapScreen(
                 )
             }
 
-            LazyRow(
-                contentPadding = PaddingValues(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.spacedBy(6.dp),
-                modifier = Modifier
-                    .padding(vertical = 8.dp)
-            ) {
-                items(categoryList) { category ->
-                    with(category) {
-                        IconChip(
-                            text = category.categoryName,
-                            selectedIconUrl = iconUrl,
-                            unSelectedIconUrl = unSelectedIconUrl,
-                            onClick = {
-                                selectedCategoryId = categoryId
-                                onCategoryClick(categoryId)
-                            },
-                            isSelected = categoryId == selectedCategoryId,
-                            isGradient = true,
-                            secondColor = SpoonyAndroidTheme.colors.white,
-                            mainColor = SpoonyAndroidTheme.colors.main400,
-                            selectedBorderColor = SpoonyAndroidTheme.colors.main200
-                        )
+            if (locationInfo.placeId == null) {
+                LazyRow(
+                    contentPadding = PaddingValues(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(6.dp),
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                ) {
+                    items(categoryList) { category ->
+                        with(category) {
+                            IconChip(
+                                text = category.categoryName,
+                                selectedIconUrl = iconUrl,
+                                unSelectedIconUrl = unSelectedIconUrl,
+                                onClick = {
+                                    selectedCategoryId = categoryId
+                                    onCategoryClick(categoryId)
+                                },
+                                isSelected = categoryId == selectedCategoryId,
+                                isGradient = true,
+                                secondColor = SpoonyAndroidTheme.colors.white,
+                                mainColor = SpoonyAndroidTheme.colors.main400,
+                                selectedBorderColor = SpoonyAndroidTheme.colors.main200
+                            )
+                        }
                     }
                 }
             }
