58b4587864462440461677e91cb4536fc4eaf85a
Hyobeen-Park <amanda0109@naver.com>
2025-06-03 17:17:18 +0900

    [FEAT/#335] authenticator 수정

--- app/src/main/java/com/spoony/spoony/core/network/TokenAuthenticator.kt
+++ app/src/main/java/com/spoony/spoony/core/network/TokenAuthenticator.kt
@@ -1,10 +1,7 @@
 package com.spoony.spoony.core.network
 
-import android.content.Context
-import android.widget.Toast
 import com.spoony.spoony.domain.repository.AuthRepository
 import com.spoony.spoony.domain.repository.TokenRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
@@ -17,45 +14,57 @@ import okhttp3.Route
 
 class TokenAuthenticator @Inject constructor(
     private val tokenRepository: TokenRepository,
-    private val authRepository: AuthRepository,
-    @ApplicationContext private val context: Context
+    private val authRepository: AuthRepository
 ) : Authenticator {
     private val tokenRefreshMutex = Mutex()
 
     override fun authenticate(route: Route?, response: Response): Request? {
         return runBlocking {
-            tokenRefreshMutex.withLock {
-                val currentAccessToken = tokenRepository.getAccessToken().firstOrNull()
-
-                val requestToken = response.request.header(AUTHORIZATION)?.removePrefix("$BEARER ")
-                if (currentAccessToken != null && requestToken != currentAccessToken) {
-                    // 이미 토큰이 갱신된 경우 다시 시도
-                    return@runBlocking response.request.newBuilder()
-                        .removeHeader(AUTHORIZATION)
-                        .addHeader(AUTHORIZATION, "$BEARER $currentAccessToken")
-                        .build()
-                }
-
-                val refreshToken = tokenRepository.getRefreshToken().firstOrNull() ?: return@runBlocking null
-
-                val result = authRepository.refreshToken(refreshToken)
-                val newToken = result.getOrNull()
-
-                if (newToken == null) {
-                    tokenRepository.clearTokens()
-                    Toast.makeText(context, "재로그인이 필요합니다", Toast.LENGTH_SHORT).show()
-                    return@runBlocking null
-                }
-
-                tokenRepository.updateTokens(newToken)
-
-                response.request.newBuilder()
-                    .removeHeader(AUTHORIZATION)
-                    .addHeader(AUTHORIZATION, "$BEARER ${newToken.accessToken}")
-                    .build()
-            }
+            handleAuthenticator(response)
         }
     }
+
+    private suspend fun handleAuthenticator(response: Response): Request? {
+        return tokenRefreshMutex.withLock {
+            val currentAccessToken = tokenRepository.getAccessToken().firstOrNull()
+            val requestToken = getRequestToken(response.request)
+
+            // 이미 토큰 재발급이 완료된 경우 -> token 바꿔서 재요청
+            if (isTokenRefreshed(requestToken, currentAccessToken)) {
+                return@withLock buildRequestWithToken(response.request, currentAccessToken.orEmpty())
+            }
+
+            // refreshToken이 없는 경우
+            val refreshToken = tokenRepository.getRefreshToken().firstOrNull() ?: return@withLock null
+
+            val newToken = authRepository.refreshToken(refreshToken).getOrNull()
+
+            // 토큰 재발급에 실패한 경우
+            if (newToken == null) {
+                tokenRepository.clearTokens()
+                return@withLock null
+            }
+
+            // 토큰 재발급 성공
+            tokenRepository.updateTokens(newToken)
+            return buildRequestWithToken(response.request, newToken.accessToken)
+        }
+    }
+
+    private fun getRequestToken(request: Request): String? =
+        request.header(AUTHORIZATION)?.removePrefix("$BEARER ")
+
+    private fun isTokenRefreshed(requestToken: String?, currentAccessToken: String?): Boolean {
+        if (requestToken == null || currentAccessToken == null) return false
+
+        return requestToken != currentAccessToken
+    }
+
+    private fun buildRequestWithToken(request: Request, token: String): Request =
+        request.newBuilder()
+            .removeHeader(AUTHORIZATION)
+            .addHeader(AUTHORIZATION, token)
+            .build()
 
     companion object {
         private const val AUTHORIZATION = "Authorization"
