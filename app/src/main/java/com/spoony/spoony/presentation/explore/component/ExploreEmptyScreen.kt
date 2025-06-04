fe86f189b8c7eb55da171cb3fc1a348c83470fbe
roel <ash49901@gmail.com>
2025-06-04 23:36:10 +0900

    [FEAT/#337] 탐색 뷰 로티 되돌리기

--- app/src/main/java/com/spoony/spoony/presentation/explore/component/ExploreEmptyScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/explore/component/ExploreEmptyScreen.kt
@@ -1,18 +1,20 @@
 package com.spoony.spoony.presentation.explore.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.spoony.spoony.R
 import com.spoony.spoony.core.designsystem.component.button.SpoonyButton
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
@@ -26,6 +28,7 @@ fun ExploreEmptyScreen(
     exploreType: ExploreType,
     modifier: Modifier = Modifier
 ) {
+    val lottieComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.spoony_explore_empty))
     val contentDescription = when (exploreType) {
         ExploreType.ALL -> "아직 발견된 장소가 없어요.\n나만의 리스트를 공유해 볼까요?"
         ExploreType.FOLLOWING -> "아직 팔로우 한 유저가 없어요.\n관심 있는 유저들을 팔로우해보세요."
@@ -40,12 +43,12 @@ fun ExploreEmptyScreen(
             .fillMaxWidth()
             .padding(top = 56.dp)
     ) {
-        Image(
-            painter = painterResource(R.drawable.img_empty_explore),
+        LottieAnimation(
             modifier = Modifier
-                .size(220.dp)
+                .padding(horizontal = 50.dp)
                 .aspectRatio(1f),
-            contentDescription = null
+            composition = lottieComposition,
+            iterations = LottieConstants.IterateForever
         )
 
         Text(
