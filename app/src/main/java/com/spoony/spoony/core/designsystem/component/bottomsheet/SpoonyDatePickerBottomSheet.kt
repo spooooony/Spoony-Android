97d4c683c0d9f9e606d66ac36671cf9a56a616cc
MinJae Han <hanminjae131@gmail.com>
2025-06-06 14:35:23 +0900

    [MOD/#334] 바텀시트 컨펌 밸류 로직 삭제

--- app/src/main/java/com/spoony/spoony/core/designsystem/component/bottomsheet/SpoonyDatePickerBottomSheet.kt
+++ app/src/main/java/com/spoony/spoony/core/designsystem/component/bottomsheet/SpoonyDatePickerBottomSheet.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
