52455b33d1de8a42ca8b4af213ba238a9320ae21
MinJae Han <hanminjae131@gmail.com>
2025-06-20 00:32:55 +0900

    [FIX/#368] 등록 화면 로딩 추가 및 리컴포지션 최적화

--- app/src/main/java/com/spoony/spoony/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/register/RegisterScreen.kt
@@ -1,7 +1,6 @@
 package com.spoony.spoony.presentation.register
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
