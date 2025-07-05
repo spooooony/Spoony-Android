0a22c869ab037feef475aede94f14b4b0cb106f7
Hyobeen-Park <amanda0109@naver.com>
2025-07-05 19:11:27 +0900

    [FEAT/#386] 로컬 트래커 세팅

--- app/src/main/java/com/spoony/spoony/presentation/MainActivity.kt
+++ app/src/main/java/com/spoony/spoony/presentation/MainActivity.kt
@@ -6,12 +6,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.CompositionLocalProvider
+import com.spoony.spoony.core.analytics.LocalTracker
+import com.spoony.spoony.core.analytics.MixPanelTracker
 import com.spoony.spoony.core.designsystem.theme.SpoonyAndroidTheme
 import com.spoony.spoony.presentation.main.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var tracker: MixPanelTracker
+
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +26,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpoonyAndroidTheme {
-                MainScreen()
+                CompositionLocalProvider(LocalTracker provides tracker) {
+                    MainScreen()
+                }
             }
         }
     }
