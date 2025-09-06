781c05d07b204e351ae00fdd2deeb254420c206c
Hyobeen-Park <amanda0109@naver.com>
2025-09-07 06:46:35 +0900

    [CHORE/#395] 온보딩 로띠 수정

--- app/src/main/java/com/spoony/spoony/presentation/auth/onboarding/OnboardingEndScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/auth/onboarding/OnboardingEndScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
@@ -87,7 +90,9 @@ private fun OnboardingEndScreen(
 
         LottieAnimation(
             composition = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.spoony_onboarding)).value,
-            iterations = LottieConstants.IterateForever
+            iterations = LottieConstants.IterateForever,
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
         )
 
         Spacer(modifier = Modifier.weight(1f))
