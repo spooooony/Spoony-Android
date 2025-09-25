49008e150526a58a8da39e30b6b7fd29bc68a9db
hanminjae <hanminjae131@gmail.com>
2025-09-25 19:18:03 +0900

    [FIX/#402] 리뷰 등록시 Reset State 제거 및 중복 클릭 방지

--- app/src/main/java/com/spoony/spoony/presentation/profileedit/ProfileEditScreen.kt
+++ app/src/main/java/com/spoony/spoony/presentation/profileedit/ProfileEditScreen.kt
@@ -2,7 +2,6 @@ package com.spoony.spoony.presentation.profileedit
 
 import BirthSelectButton
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
